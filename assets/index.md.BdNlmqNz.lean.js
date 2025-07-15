22ce7cb415947049c54d518dbbc9d00f213012ad
mzgajner <mato@zgajner.com>
2025-07-15 20:30:24 +0000

    Deploying to gh-pages from @ mzgajner/jejsti@ea87358aaa4e0a1bb654383ccbbcbfe72319e85b 🚀

---
+++ assets/index.md.BdNlmqNz.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,C as a,c as n,o,G as r}from"./chunks/framework.CyzmdYZ6.js";const x=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"page"},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),c={name:"index.md"};function s(d,i,p,_,l,m){const e=a("Search");return o(),n("div",null,[r(e)])}const h=t(c,[["render",s]]);export{x as __pageData,h as default};
